51377dab90b0b60d0e5fd366bb1e0de530cb7a21
王辉 <y1wanghui@163.com>
2019-06-13 17:16:11 +0800

    增加csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\User;
 use Illuminate\Http\Request;
-
 class UsersController extends Controller
 {
     //注册页面
@@ -18,5 +16,16 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        //用户提交后处理
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email'=> 'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
