da05fb197e8c057a4096d5291f6c2b0e026f86af
戰為十曉 <y1wanghui@163.com>
2019-07-03 17:54:28 +0800

    remember me

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,7 +20,7 @@ class SessionsController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        if (Auth::attempt($credentials)){
+        if (Auth::attempt($credentials,$request->has('remember'))){
             //登录成功后的相关操作
             session()->flash('success','欢迎回来');
             return redirect()->route('users.show',[Auth::user()]);
