faa8092389701befbeed99a283186f938276146f
戰為十曉 <y1wanghui@163.com>
2019-07-08 17:02:15 +0800

    微博数据激活模型

---
+++ app/Models/Status.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    //一条微博属于一个用户
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    //yi dui duo
+    public function statues()
+    {
+        return $this->hasMany(Status::class);
+    }
+}
---
+++ database/migrations/2019_07_08_084521_create_statues_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('user_id')->index();
+            $table->index(['created_at']);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statues');
+    }
+}
